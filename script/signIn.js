fa19fcf8f37e5e50e76521300bd142934bd1d50d
Daniel Putzer <putzer.daniel@rolmail.net>
2017-07-19 00:08:16 +0200

    SignIn & SignUp created
    Created SignIn & SignUp function with Firebase. SignUp also stores User's First- and Lastname in the Realtime Database.

---
+++ script/signIn.js
@@ -0,0 +1,48 @@
+(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-23wnCzHAmW_AaS_yODHhcTbh9RhnbLY",
+    authDomain: "weekz-fba03.firebaseapp.com",
+    databaseURL: "https://weekz-fba03.firebaseio.com",
+    projectId: "weekz-fba03",
+    storageBucket: "gs://weekz-fba03.appspot.com/",
+    messagingSenderId: "708230896117"
+  };
+  firebase.initializeApp(config);
+
+  // Get Elements
+  const txtEmail = document.getElementById('email');
+  const txtPassword = document.getElementById('password');
+  const btnSignin = document.getElementById('signin');
+
+  // Login Event
+  btnSignin.addEventListener('click', e => {
+
+    // Get Email & Password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    if (email == "") {
+      txtEmail.style.border = "1px solid #FF514C";
+      txtPassword.style.border = "none";
+    } else if (pass == "") {
+      txtPassword.style.border = "1px solid #FF514C";
+      txtEmail.style.border = "none";
+    } else {
+      // Sign In
+      const promise = auth.signInWithEmailAndPassword(email, pass)
+        .then(function(response) {
+          txtEmail.style.border = "1px solid #7AC843"
+          txtPassword.style.border = "1px solid #7AC843"
+        });
+      promise.catch(e => {
+        txtEmail.style.border = "1px solid #FF514C";
+        txtPassword.style.border = "1px solid #FF514C";
+        
+        console.log(e.message)
+      });
+    }
+  });
+}());
---
+++ script/signUp.js
@@ -0,0 +1,94 @@
+
+(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-23wnCzHAmW_AaS_yODHhcTbh9RhnbLY",
+    authDomain: "weekz-fba03.firebaseapp.com",
+    databaseURL: "https://weekz-fba03.firebaseio.com",
+    projectId: "weekz-fba03",
+    storageBucket: "gs://weekz-fba03.appspot.com/",
+    messagingSenderId: "708230896117"
+  };
+  firebase.initializeApp(config);
+
+  // Get Elements
+  const txtFirstname = document.getElementById('first');
+  const txtLastname = document.getElementById('last');
+  const txtEmail = document.getElementById('email');
+  const txtPassword1 = document.getElementById('password1');
+  const txtPassword2 = document.getElementById('password2');
+  const btnSignup = document.getElementById('signup');
+
+  // Login Event
+  btnSignup.addEventListener('click', e => {
+
+    // Get Email & Password
+    const first = txtFirstname.value;
+    const last = txtLastname.value;
+    const email = txtEmail.value;
+    const pass1 = txtPassword1.value;
+    const pass2 = txtPassword2.value;
+
+    const auth = firebase.auth();
+    const ref = firebase.database().ref();
+
+    if (first == "") {
+      txtFirstname.style.border = "1px solid #FF514C";
+      txtLastname.style.border = "none";
+      txtEmail.style.border = "none";
+      txtPassword1.style.border = "none";
+      txtPassword2.style.border = "none";
+    } else if (last == "") {
+      txtLastname.style.border = "1px solid #FF514C";
+      txtFirstname.style.border = "none";
+      txtEmail.style.border = "none";
+      txtPassword1.style.border = "none";
+      txtPassword2.style.border = "none";
+    } else if (email == "") {
+      txtEmail.style.border = "1px solid #FF514C";
+      txtFirstname.style.border = "none";
+      txtLastname.style.border = "none";
+      txtPassword1.style.border = "none";
+      txtPassword2.style.border = "none";
+    } else if (pass1 == "") {
+      txtPassword1.style.border = "1px solid #FF514C";
+      txtFirstname.style.border = "none";
+      txtLastname.style.border = "none";
+      txtEmail.style.border = "none";
+      txtPassword2.style.border = "none";
+    } else if (pass2 == "") {
+      txtPassword2.style.border = "1px solid #FF514C";
+      txtFirstname.style.border = "none";
+      txtLastname.style.border = "none";
+      txtEmail.style.border = "none";
+      txtPassword1.style.border = "none";
+    } else {
+      txtFirstname.style.border = "none";
+      txtLastname.style.border = "none";
+      txtEmail.style.border = "none";
+      txtPassword1.style.border = "none";
+      txtPassword2.style.border = "none";
+
+      if (pass1 == pass2) {
+        // Sign up
+        const promise = auth.createUserWithEmailAndPassword(email, pass2)
+          .then(function(response) {
+            txtFirstname.style.border = "1px solid #7AC843"
+            txtLastname.style.border = "1px solid #7AC843"
+            txtEmail.style.border = "1px solid #7AC843"
+            txtPassword1.style.border = "1px solid #7AC843"
+            txtPassword2.style.border = "1px solid #7AC843"
+
+            ref.child("users").child(response.uid).child("info").set({firstname: first, lastname: last});
+          });
+        promise.catch(e => console.log(e.message));
+
+      } else {
+        txtPassword1.style.border = "1px solid #FF514C"
+        txtPassword2.style.border = "1px solid #FF514C"
+        console.log("Passwords dont match");
+      };
+    };
+  });
+}());
