548499c73bf0d841733fccd4fa3288f4f840c037
Daniel Putzer <putzer.daniel@rolmail.net>
2017-08-01 18:05:48 +0200

    Implemented Context Menu Design
    Front edn code for the context menu is almost done, need to create hover effect on context buttons.

--- scrpt/loadData.js
+++ scrpt/loadData.js
@@ -8,6 +8,8 @@ var dayDifference = 0;
 // SVG for Delete Button
 var deleteBtnIcon = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 
+var editBtnIcon = '<img src="res/edit.png">'
+var moveBtnIcon = '<img src="res/move.png">'
 // Get Elements
 const nameLabel = document.getElementById('userName');
 const mailLabel = document.getElementById('userEmail');
@@ -15,6 +17,7 @@ const addWeekBtn = document.getElementById('newWeek');
 const previousBtn = document.getElementById('previous');
 const allLists = document.getElementsByClassName('taskList');
 const verifyBtn = document.getElementById('verify');
+const contextMenu = document.getElementById('contextMenu');
 
 (function() {
 
@@ -71,9 +74,9 @@ const verifyBtn = document.getElementById('verify');
                 check.type = "checkbox";
                 check.classList.add("option-input", "checkbox");
 
-                var deleteBtn = document.createElement('button');
-                deleteBtn.id = "delete"
-                deleteBtn.innerHTML = deleteBtnIcon;
+                var contextBtn = document.createElement('button');
+                contextBtn.id = "context";
+                contextBtn.style.background = "context.png"
                 var p = document.createElement('p');
                 p.innerText = taskid.val().taskName;
 
@@ -88,9 +91,31 @@ const verifyBtn = document.getElementById('verify');
                 detailP.innerText = taskid.val().taskDesc;
                 detailDiv.appendChild(detailP);
 
-                li.appendChild(deleteBtn);
+                var contextDiv = document.createElement('div');
+                contextDiv.classList.add('contextMenu');
+                contextDiv.style.display = "none";
+
+                var deleteBtn = document.createElement('button');
+                deleteBtn.id = "deleteNew"
+                deleteBtn.innerHTML = deleteBtnIcon;
+
+                var editBtn = document.createElement('button');
+                editBtn.id = "edit";
+                editBtn.innerHTML = editBtnIcon;
+
+                var moveBtn = document.createElement('button')
+                moveBtn.id = "move";
+                moveBtn.innerHTML = moveBtnIcon;
+
+                contextDiv.appendChild(deleteBtn);
+                contextDiv.appendChild(editBtn);
+                contextDiv.appendChild(moveBtn);
+
+
+                li.appendChild(contextBtn);
                 li.appendChild(check);
                 li.appendChild(p);
+                li.appendChild(contextDiv);
                 li.appendChild(detailDiv);
                 li.id = taskid.key;
 
@@ -99,21 +124,37 @@ const verifyBtn = document.getElementById('verify');
                 var rightList = document.getElementById(addDay);
                 rightList.appendChild(li);
 
-                // Make Items deletable
-                deleteBtn.addEventListener('click', removeTask);
                 // Change appearance of checked task
                 check.addEventListener('click', taskDone);
                 // Show Detail View
                 p.addEventListener('click', showDetail);
 
+                // Open Context Menu and configure its buttons
+                contextBtn.addEventListener('click', toggleContext);
+                  deleteBtn.addEventListener('click', removeTask)
+
                 // Remove Task
                 function removeTask() {
-                  var task = this.parentNode;
+                  var task = this.parentNode.parentNode;
                   var id = task.id;
                   var list = task.parentNode;
 
                   list.removeChild(task);
                   dataRef.child(addDay).child(id).remove();
+                };
+
+                function toggleContext() {
+                  var task = this.parentNode;
+                  var id = task.id;
+                  var taskContext = task.children[3];
+
+                  if (taskContext.style.display == "none") {
+                    taskContext.style.display = "block";
+                    this.style.opacity = 1;
+                  } else {
+                    taskContext.style.display = "none"
+                    this.style.opacity = 0;
+                  }
                 };
 
                 // Mark Task as done
