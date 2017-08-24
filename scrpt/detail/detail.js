a637136f1df46ff5ed85e92a15359d5615e1042a
Aaron Penn <Aaron.Penn@technoalpin.com>
2017-08-24 11:15:28 +0200

    -project detail view updates in realtime when checking tasks -tasks in project detail view are now draggable into a recycle bin

--- scrpt/detail/detail.js
+++ scrpt/detail/detail.js
@@ -1,11 +1,12 @@
 var projectName = document.getElementById('projectName');
 var projectSummary = document.getElementById('projectSummary');
 var projectDesc = document.getElementById('projectDesc');
-var tasksCompleted = document.getElementById('tasksCompleted');
-var tasksOutstanding = document.getElementById('tasksOutstanding');
+var tasksCompletedText = document.getElementById('tasksCompleted');
+var tasksOutstandingText = document.getElementById('tasksOutstanding');
 var progressPercent = document.getElementById('progressPercent');
 var progressBar = document.getElementById('projectProgress');
-
+var recycleContainer = document.getElementById('recyleContainer');
+var taskContainerDiv = document.getElementsByClassName('tasks')[0];
 console.log(progressBar);
 var taskList = document.getElementById('taskList');
 var taskDots = document.getElementsByClassName('dot');
@@ -20,21 +21,34 @@ addLoggedInHandler(function(){
     dataRefProject = firebase.database().ref().child('users').child(userId).child('projects');
     dataRefTasks = firebase.database().ref().child('users').child(userId).child('tasks');
 
+    taskContainerDiv.ondragover = function(ev)
+    {
+      ev.preventDefault();
+    };
+    recycleContainer.ondrop = elementDropped;
+
     loadDetailData(projectID);
 
 });
 
-function generateProjectDetails(title, summary, description, color, taskCount, tasksDone) {
+function updateProjectDetails(title, summary, description, color, taskCount, tasksDone) {
   projectName.innerText = title;
   projectSummary.innerText = summary;
   projectDesc.innerText = description;
-
-  tasksCompleted.innerText = tasksDone;
-  tasksOutstanding.innerText = taskCount - tasksDone;
-
-  console.log(progressBar);
-
   progressBar.classList.add(color);
+
+  updateProjectProgess(taskCount, tasksDone);
+
+  for (var i = 0; i < taskDots.length; i++) {
+    taskDots[i].classList.add(color);
+  }
+}
+
+function updateProjectProgess(taskCount, tasksDone)
+{
+  tasksCompletedText.innerText = tasksDone;
+  tasksOutstandingText.innerText = taskCount - tasksDone;
+  
   var percentage;
   if (taskCount > 0)
     percentage = Math.floor(tasksDone * 100 / taskCount);
@@ -43,10 +57,6 @@ function generateProjectDetails(title, summary, description, color, taskCount, t
 
   progressBar.style.width = percentage + "%";
   progressPercent.innerText = percentage;
-
-  for (var i = 0; i < taskDots.length; i++) {
-    taskDots[i].classList.add(color);
-  }
 }
 
 function generateProjectTask(name, desc, check, color, id) {
@@ -54,6 +64,9 @@ function generateProjectTask(name, desc, check, color, id) {
   li = document.createElement('li');
   li.className = "task";
   li.id = id;
+  li.draggable = true;
+  li.ondragstart= dragStarted;
+  li.ondragend = dragStopped;
 
   var dot = document.createElement('div');
   dot.classList.add('dot', color);
@@ -83,14 +96,39 @@ function generateProjectTask(name, desc, check, color, id) {
 //Toggle checkbox
 function toggleTaskDone() {
     var task = this.parentNode;
-    console.log(task);
-    var thisList = task.parentNode;
-    var id = task.id;
-    console.log(id);
     var taskText = task.children[1];
-    var status = dataRefTasks.child(id).once('value').then(function(checked) {
-    var value = checked.val().checked;
+    //var thisList = task.parentNode;
+    var newCheckState = this.checked;
+
     taskText.classList.toggle('toggle');
-    var update = dataRefTasks.child(id).update({checked: !value});
-  });
+    var update = dataRefTasks.child(task.id).update({checked: newCheckState});
+    
+    if (newCheckState)
+      tasksCompleted++;
+    else
+      tasksCompleted--;
+
+    updateProjectProgess(taskCount, tasksCompleted);
+
+}
+
+function dragStarted(ev)
+{
+  this.style.opacity = "0.2";
+  recycleContainer.children[0].src = "res/removeRed.png";
+  recycleContainer.style.opacity = "1";
+  ev.dataTransfer.setData("text", this.id);
+}
+
+function dragStopped(ev)
+{
+  recycleContainer.children[0].src = "res/remove.png";
+  recycleContainer.style.opacity = "0.1";
+  this.style.opacity = "1";
+}
+function elementDropped(ev) 
+{
+  var itemId = ev.dataTransfer.getData("text");
+  var item = document.getElementById(itemId);
+  taskList.removeChild(item);
 }
