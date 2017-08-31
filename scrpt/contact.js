1f46e6bb725a92c091e2036912b5db872e4d47ff
Daniel Putzer <putzer.daniel@rolmail.net>
2017-08-31 15:12:46 +0200

    Major Front-End Changes + Additions
    Several design changes on the index page.
    Pictures have been updated to show the latest UI version.
    Contact form with PHP function has been added.

---
+++ scrpt/contact.js
@@ -0,0 +1,17 @@
+// Get Elements
+var barSignIn = document.getElementById('barSignIn');
+var barSignUp = document.getElementById('barSignUp');
+var barDash = document.getElementById('barDash');
+
+addLoggedInHandler(function() {
+  barDash.style.display = "block";
+  barSignIn.style.display = "none";
+  barSignUp.style.display = "none";
+});
+
+var sendBtn = document.getElementById('submit');
+
+sendBtn.addEventListener('click', function() {
+  submit.style.background = '#0BE370';
+  submit.innerText = 'SENT';
+});
