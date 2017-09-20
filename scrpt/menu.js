f772261a9ce57e687f6366bcd49b5d2973e738cb
Daniel Putzer <putzer.daniel@rolmail.net>
2017-09-20 15:15:23 +0200

    Menu Rework
    Design changes to the menu.

--- scrpt/menu.js
+++ scrpt/menu.js
@@ -5,6 +5,9 @@ var open = document.getElementById('openMenuBtn');
 var close = document.getElementById('closeMenuBtn');
 var signOut = document.getElementById('signOut');
 
+var youBtn = document.getElementById('openYouBtn');
+var youDiv = document.getElementById('youDiv');
+
 var nameLabel = document.getElementById('userName');
 var mailLabel = document.getElementById('userEmail');
 var verifyBtn = document.getElementById('verify');
@@ -35,12 +38,10 @@ function loadUserInfo() {
     nameLabel.innerText = first + " " + last;
     mailLabel.innerText = mail;
 
-    nameLabel.style.opacity = "0";
-    mailLabel.style.opacity = "0";
-
   });
 }
 function linkMenuEventListener() {
+
   //Verify E-Mail button
   verifyBtn.addEventListener('click', function () {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -48,6 +49,20 @@ function linkMenuEventListener() {
       verifyBtn.innerText = "EMAIL SENT";
     });
   });
+
+  // Toggle You
+  youBtn.addEventListener('click', function() {
+    console.log(youBtn);
+    if (youDiv.style.display == "block") {
+      youDiv.style.display = "none";
+      console.log("trottl");
+    } else {
+      youDiv.style.display = "block"
+      console.log(youDiv);
+      console.log("trottl");
+    }
+  });
+
   // Open Menu
   open.addEventListener('click', function () {
     back.style.zIndex = "5000";
@@ -66,11 +81,11 @@ function linkMenuEventListener() {
 
   // Close Menu
   close.addEventListener('click', function () {
-    nameLabel.style.transition = "0.2s";
-    mailLabel.style.transition = "0.2s";
-
-    nameLabel.style.opacity = "0";
-    mailLabel.style.opacity = "0";
+    // nameLabel.style.transition = "0.2s";
+    // mailLabel.style.transition = "0.2s";
+    //
+    // nameLabel.style.opacity = "0";
+    // mailLabel.style.opacity = "0";
 
     back.style.opacity = "0";
 
